e687fd41786738bd1871bb6ddb1fb84b561a5744
singhjptech <singhjptech@gmail.com>
2021-02-05 12:40:54 +0000

    new api linked

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-// Get Quote from API
-async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        getQuote();
-        console.log('whoops, no quote', error);        
-    }
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from Api
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
 }
 
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch error here
+    }
+}
 //On Load
-getQuote();
+getQuotes();
